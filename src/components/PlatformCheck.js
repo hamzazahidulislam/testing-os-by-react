abfba538cb5cbe2654b6d9775c4976cdd137b844
Hamza Zahidul Islam <hamzazahidulislam444@gmail.com>
2025-10-08 12:45:43 +0600

    add: platform check

--- src/components/PlatformCheck.js
+++ src/components/PlatformCheck.js
@@ -4,8 +4,9 @@ export default function PlatformCheck() {
   const userAgent = navigator.userAgent || navigator.vendor || ''
   const isAndroid = /Android/i.test(userAgent)
   const isWebView = /\bwv\b/.test(userAgent)
+  const isMac = /Mac/i.test(userAgent)
   const isAndroidWebView = isAndroid && isWebView
-
+  console.log(userAgent)
   return (
     <div className='p-4'>
       {isAndroidWebView ? (
@@ -13,8 +14,10 @@ export default function PlatformCheck() {
       ) : isAndroid ? (
         <p>You are using Android Browser 🌐</p>
       ) : (
-        <p>You are using Desktop or Other Platform 💻</p>
+        <p>You are using Desktop or Other Platform 💻 </p>
       )}
+      {isMac ? <h1> You are using Mac </h1> : <h1> You are not using mac </h1>}
+      <h1>{navigator.platform} </h1>
     </div>
   )
 }
